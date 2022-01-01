12d97a2d3543ba840c5b7cc7577fa3d594dbd749
Koray Çiçekçioğulları <k.cicekciogullari@gmail.com>
2022-01-02 01:58:28 +0300

    Creating Layout (Completed). - https://trello.com/c/C1nxp3fY

---
+++ components/guest/ContentAndSidebarComponent.vue
@@ -0,0 +1,238 @@
+<template>
+  <div class="container mx-auto">
+    <div class="grid grid-cols-4 gap-4">
+      <div class="col-span-3">
+        <div class="w-full py-4">
+          <div class="w-full py-4 px-8 mb-4 text-white bg-gradient-to-r from-purple-900 via-purple-700 to-purple-900 relative rounded">
+            <h3 class="font-semibold">Popüler Yazılar</h3>
+            <icons-trend-icon class="absolute w-14 h-14 top-0 right-2 fill-current z-0 opacity-10" />
+          </div>
+          <div class="flex gap-4 items-center">
+            <div>
+              <a href="">
+                <figure class="w-72 overflow-hidden rounded">
+                  <img class="w-full h-full object-cover" src="https://images.pexels.com/photos/3183197/pexels-photo-3183197.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" width="100%" height="100%" alt="">
+                </figure>
+              </a>
+            </div>
+            <div class="flex-shrink">
+              <a href="" class="block font-semibold text-lg mb-2 transition hover:text-purple-800">Hotel Review: DoubleTree by Hilton, Bath</a>
+              <p class="text-sm text-gray-500 mb-4">This is important to emphasize, because PayPal isn’t adding Bitcoin as a currency to its own digital wallet. It also won’t be processing Bitcoing payments on its secure payments platform: everything is being handled by one of the three third-parties.</p>
+              <div class="flex gap-4 text-xs font-semibold text-gray-400">
+                <a href="" class="flex gap-1 items-center">
+                  <lazy-icons-tag-icon class="w-4 h-4 fill-current"/>
+                  Teknoloji
+                </a>
+                <a href="" class="flex gap-1 items-center">
+                  <lazy-icons-clock-icon class="w-4 h-4 fill-current"/>
+                  01 Ocak 2022
+                </a>
+                <a href="" class="flex gap-1 items-center">
+                  <lazy-icons-user-icon class="w-4 h-4 fill-current"/>
+                  John Doe
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="w-full py-4">
+          <div class="flex gap-4 items-center">
+            <div>
+              <a href="">
+                <figure class="w-72 overflow-hidden rounded">
+                  <img class="w-full h-full object-cover" src="https://images.pexels.com/photos/326501/pexels-photo-326501.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" width="100%" height="100%" alt="">
+                </figure>
+              </a>
+            </div>
+            <div>
+              <a href="" class="block font-semibold text-lg mb-2 transition hover:text-purple-800">Monday be like, Those bikes had a Rough Monday too</a>
+              <p class="text-sm text-gray-500 mb-4">Merchants who pre-sell products, meaning asking for money up-front for a product or service that will be delivered in the future, will also not be supported. This is, according to PayPal, to safeguard customers from businesses that can’t give refunds if they fold before the product is shipped and after buyer protection expires.</p>
+              <div class="flex gap-4 text-xs font-semibold text-gray-400">
+                <a href="" class="flex gap-1 items-center">
+                  <lazy-icons-tag-icon class="w-4 h-4 fill-current"/>
+                  Teknoloji
+                </a>
+                <a href="" class="flex gap-1 items-center">
+                  <lazy-icons-clock-icon class="w-4 h-4 fill-current"/>
+                  01 Ocak 2022
+                </a>
+                <a href="" class="flex gap-1 items-center">
+                  <lazy-icons-user-icon class="w-4 h-4 fill-current"/>
+                  John Doe
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="w-full py-4">
+          <div class="flex gap-4 items-center">
+            <div>
+              <a href="">
+                <figure class="w-72 overflow-hidden rounded">
+                  <img class="w-full h-full object-cover" src="https://images.pexels.com/photos/3825569/pexels-photo-3825569.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" width="100%" height="100%" alt="">
+                </figure>
+              </a>
+            </div>
+            <div>
+              <a href="" class="block font-semibold text-lg mb-2 transition hover:text-purple-800">Office space with a Mac computer in 2020</a>
+              <p class="text-sm text-gray-500 mb-4">This is important to emphasize, because PayPal isn’t adding Bitcoin as a currency to its own digital wallet. It also won’t be processing Bitcoing payments on its secure payments platform: everything is being handled by one of the three third-parties.</p>
+              <div class="flex gap-4 text-xs font-semibold text-gray-400">
+                <a href="" class="flex gap-1 items-center">
+                  <lazy-icons-tag-icon class="w-4 h-4 fill-current"/>
+                  Teknoloji
+                </a>
+                <a href="" class="flex gap-1 items-center">
+                  <lazy-icons-clock-icon class="w-4 h-4 fill-current"/>
+                  01 Ocak 2022
+                </a>
+                <a href="" class="flex gap-1 items-center">
+                  <lazy-icons-user-icon class="w-4 h-4 fill-current"/>
+                  John Doe
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="w-full py-4">
+          <div class="flex gap-4 items-center">
+            <div>
+              <a href="">
+                <figure class="w-72 overflow-hidden rounded">
+                  <img class="w-full h-full object-cover" src="https://images.pexels.com/photos/3692887/pexels-photo-3692887.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" width="100%" height="100%" alt="">
+                </figure>
+              </a>
+            </div>
+            <div>
+              <a href="" class="block font-semibold text-lg mb-2 transition hover:text-purple-800">Together with red and white bakers twine</a>
+              <p class="text-sm text-gray-500 mb-4">In other words, enabling the new Mobile setting (Chrome Management needs to be turned on first) means that all supported policies (check the lightbulb beside each policy to see if it will on Android and iOS) will apply to Chrome on mobile devices. All users who are signed in to Chrome for mobile with their organization’s account.</p>
+              <div class="flex gap-4 text-xs font-semibold text-gray-400">
+                <a href="" class="flex gap-1 items-center">
+                  <lazy-icons-tag-icon class="w-4 h-4 fill-current"/>
+                  Teknoloji
+                </a>
+                <a href="" class="flex gap-1 items-center">
+                  <lazy-icons-clock-icon class="w-4 h-4 fill-current"/>
+                  01 Ocak 2022
+                </a>
+                <a href="" class="flex gap-1 items-center">
+                  <lazy-icons-user-icon class="w-4 h-4 fill-current"/>
+                  John Doe
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-full py-4">
+        <div class="w-full py-4 px-8 mb-4 text-white bg-gradient-to-r from-orange-500 via-orange-400 to-orange-500 relative rounded">
+          <h3 class="font-semibold">Kategoriler</h3>
+          <lazy-icons-category-icon class="absolute w-14 h-14 top-0 right-2 fill-current z-0 opacity-10" />
+        </div>
+        <nav class="font-semibold">
+          <ul>
+            <li class="mb-1">
+              <a href="" class="block py-4 px-8 rounded relative overflow-hidden group">
+                <span class="relative z-10">Teknoloji</span>
+                <figure class="absolute left-0 top-0 overflow-hidden z-0 grayscale opacity-10 transition-all hover:grayscale-0 hover:opacity-30">
+                  <img class="w-full object-cover" src="https://images.pexels.com/photos/1432675/pexels-photo-1432675.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" width="100%" height="100%" alt="">
+                </figure>
+              </a>
+            </li>
+            <li class="mb-1">
+              <a href="" class="block py-4 px-8 rounded relative overflow-hidden group">
+                <span class="relative z-10">Eğitim</span>
+                <figure class="absolute left-0 top-0 overflow-hidden z-0 grayscale opacity-10 transition-all hover:grayscale-0 hover:opacity-30">
+                  <img class="w-full object-cover" src="https://images.pexels.com/photos/147413/twitter-facebook-together-exchange-of-information-147413.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" width="100%" height="100%" alt="">
+                </figure>
+              </a>
+            </li>
+            <li class="mb-1">
+              <a href="" class="block py-4 px-8 rounded relative overflow-hidden group">
+                <span class="relative z-10">Sağlık</span>
+                <figure class="absolute left-0 top-0 overflow-hidden z-0 grayscale opacity-10 transition-all hover:grayscale-0 hover:opacity-30">
+                  <img class="w-full object-cover" src="https://images.pexels.com/photos/3692887/pexels-photo-3692887.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" width="100%" height="100%" alt="">
+                </figure>
+              </a>
+            </li>
+            <li class="mb-1">
+              <a href="" class="block py-4 px-8 rounded relative overflow-hidden group">
+                <span class="relative z-10">Gündem</span>
+                <figure class="absolute left-0 top-0 overflow-hidden z-0 grayscale opacity-10 transition-all hover:grayscale-0 hover:opacity-30">
+                  <img class="w-full object-cover" src="https://images.pexels.com/photos/1036936/pexels-photo-1036936.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" width="100%" height="100%" alt="">
+                </figure>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div class="w-full py-4 px-8 mt-4 mb-4 text-white bg-gradient-to-r from-orange-500 via-orange-400 to-orange-500 relative rounded">
+          <h3 class="font-semibold">En Son Yorumlar</h3>
+          <lazy-icons-comment-icon class="absolute w-14 h-14 top-0 right-2 fill-current z-0 opacity-10" />
+        </div>
+        <div>
+          <div class="flex gap-4 mb-4">
+            <div class="flex items-center gap-4">
+              <div class="w-14">
+                <figure class="w-14 h-14 overflow-hidden rounded-full">
+                  <img class="w-full h-full object-cover" width="100%" height="100%" src="https://images.pexels.com/photos/7983603/pexels-photo-7983603.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+                </figure>
+              </div>
+              <div class="text-sm">
+                <strong class="font-semibold">John Doe</strong>
+                <p class="text-xs text-sm text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima quasi, voluptates.</p>
+              </div>
+            </div>
+          </div>
+          <div class="flex gap-4 mb-4">
+            <div class="flex items-center gap-4">
+              <div class="w-14">
+                <figure class="w-14 h-14 overflow-hidden rounded-full">
+                  <img class="w-full h-full object-cover" width="100%" height="100%" src="https://images.pexels.com/photos/9064774/pexels-photo-9064774.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+                </figure>
+              </div>
+              <div class="text-sm">
+                <strong class="font-semibold">John Doe</strong>
+                <p class="text-xs text-sm text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima quasi, voluptates.</p>
+              </div>
+            </div>
+          </div>
+          <div class="flex gap-4 mb-4">
+            <div class="flex items-center gap-4">
+              <div class="w-14">
+                <figure class="w-14 h-14 overflow-hidden rounded-full">
+                  <img class="w-full h-full object-cover" width="100%" height="100%" src="https://images.pexels.com/photos/3763188/pexels-photo-3763188.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+                </figure>
+              </div>
+              <div class="text-sm">
+                <strong class="font-semibold">John Doe</strong>
+                <p class="text-xs text-sm text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima quasi, voluptates.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="w-full py-4 px-8 mt-4 mb-4 text-white bg-gradient-to-r from-orange-500 via-orange-400 to-orange-500 relative rounded">
+          <h3 class="font-semibold">Editörün Seçtiği</h3>
+          <lazy-icons-cup-icon class="absolute w-14 h-14 top-0 right-2 fill-current z-0 opacity-10" />
+        </div>
+        <div>
+          <div>
+            <a href="">
+              <figure class="">
+                <img class="w-full h-40 object-cover rounded mb-4" src="https://images.pexels.com/photos/147413/twitter-facebook-together-exchange-of-information-147413.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" width="100%" height="100%" alt="">
+                <figcaption>
+                  <h2 class="font-semibold text-lg mb-2 transition hover:text-purple-800">Security Isn’t Just a Technology Problem</h2>
+                  <p class="text-sm text-gray-500">“Herobrine” is an urban legend associated with Minecraft, who first appeared as a single image on 4chan’s /v/ board. According to rumors, Herobrine appears in players’ worlds and builds strange constructions.</p>
+                </figcaption>
+              </figure>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContentAndSidebarComponent"
+}
+</script>
